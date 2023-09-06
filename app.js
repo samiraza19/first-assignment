e12df9066be06f2502d3eeccb2ea000f8aa6768c
=SamiRaza <=samirazagb@gmail.com>
2023-09-06 22:24:40 +0500

    =work complete

---
+++ app.js
@@ -0,0 +1,40 @@
+// CHP 1
+
+// Q 1
+alert("First Name");
+
+alert("Last Name");
+
+alert("Email");
+
+alert("Phone Number ");
+
+alert("Password");
+
+// Q 2
+alert("You are learning JavaScript");
+
+alert("Welcome To The Another Good Day")
+// CHP 1 Ends Here
+
+// CHP 2
+
+// Q 1
+var markStarc ="";
+
+// Q 2
+
+var firstName;
+firstName = "Salam";
+
+console.log("Salam")
+
+
+// Q 3
+var teamName ="Gojaba11";
+alert("Gojaba11")
+
+// Q 4
+var goodPerson = "michel";
+alert("michel")
+// CHP 2 Ends Here
