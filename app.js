0d801bad53f9875dbe6b1c90184721e09d974dc1
=SamiRaza <=samirazagb@gmail.com>
2023-09-06 22:25:31 +0500

    =work complete

--- app.js
+++ app.js
@@ -38,3 +38,89 @@ alert("Gojaba11")
 var goodPerson = "michel";
 alert("michel")
 // CHP 2 Ends Here
+
+// CHP 3
+
+// Q 1
+var caseQty ="144"
+console.log("144")
+
+var num = 9
+console.log ("9")
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log ("30")
+
+
+// Q 2
+var merchTotal = 100;
+var shipingCharge = 10;
+var orderTotal = merchTotal + shipingCharge;
+console.log ("110")
+
+var num1 = 3;
+var num2 = 5;
+
+var res = num1 + num2;
+console.log("8")
+// CHP 3 Ends Here
+
+// CHP 4
+
+// Q 1
+var productCost = 3.45;
+
+// Q 2
+var nameofBrand = "xyz";
+
+// Q 3
+var sami001 = 1920;
+console.log ("1920")
+
+// Q 4
+var firstName ="Muhammad";
+var lastName ="Sami";
+var res = firstName + lastName;
+console.log("Muhammad Sami")
+
+// Q 5
+// List Of Legal Variables
+sami001
+caseQty
+goodPerson
+
+// List Of illegal Variables
+// 001sami
+// #@abc
+// seq@11
+
+// CHP 4 Ends Here
+
+// CHP 5
+
+// Q 1
+var calc = 66/7;
+console.log ("9.4285714285714")
+
+// Q 2
+var calc =20/6;
+console.log ("3.33333333333333")
+
+// Q 3
+var calc =1000*2000;
+console.log ("2000000")
+
+// Q 4
+var calc =50-8;
+console.log("42")
+
+// Q 5
+var calc =10/8;
+console.log("2")
+
+// Q 6
+var calc =6*3;
+console.log("18")
+// CHP 5 Ends Here
